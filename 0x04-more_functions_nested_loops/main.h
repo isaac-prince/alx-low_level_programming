a00a246a645488348bced89a9d4185d2ba0c29f8
isaac-prince <isaacsekats97@gmail.com>
2022-09-15 15:51:22 -0700

    changes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,43 +1,30 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef _HOLBERTON_H_
+
+#define _HOLBERTON_H_
 
 
 
 int _putchar(char c);
 
-void print_alphabet(void);
+int _isupper(int c);
 
-void print_alphabet_x10(void);
+int _isdigit(int c);
 
-int _islower(int c);
+int mul(int a, int b);
 
-int _isalpha(int c);
+void print_most_numbers(void);
 
-int print_sign(int n);
+void more_numbers(void);
 
-int _abs(int);
+void print_line(int n);
 
-int print_last_digit(int);
+void print_diagonal(int n);
 
-void jack_bauer(void);
+void print_square(int size);
 
-void times_table(void);
+void print_triangle(int size);
 
-void pr_digit(int charcode, int fmt);
-
-int add(int a, int b);
-
-void pr_with_fmt(int charcode, int fmt);
-
-void print_to_98(int n);
-
-void pr_3_digit(int charcode, int fmt);
-
-void print_times_table(int n);
-
-void print_spaces(int n);
-
-void print_zero(void);
+void print_numbers(void);
 
 
 
